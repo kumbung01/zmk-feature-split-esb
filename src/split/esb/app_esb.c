edd66ae2bd1022ef89241b064fb35f7817c012ce
seung <kumbung01@naver.com>
2025-08-27 22:50:16 +0900

    disable reque

--- src/split/esb/app_esb.c
+++ src/split/esb/app_esb.c
@@ -60,8 +60,8 @@ K_MSGQ_DEFINE(m_msgq_tx_payloads, sizeof(struct esb_payload),
               CONFIG_ZMK_SPLIT_ESB_PROTO_MSGQ_ITEMS, 4);
 
 // Define a buffer of payloads to store TX payloads in between timeslots
-K_MSGQ_DEFINE(m_msgq_tx_payloads_sent, sizeof(struct esb_payload), 
-              CONFIG_ZMK_SPLIT_ESB_PROTO_MSGQ_ITEMS, 4);
+// K_MSGQ_DEFINE(m_msgq_tx_payloads_sent, sizeof(struct esb_payload), 
+//               CONFIG_ZMK_SPLIT_ESB_PROTO_MSGQ_ITEMS, 4);
 
 // K_TIMER_DEFINE(tx_retry_timer, tx_retry_callback, NULL);
 
@@ -104,7 +104,7 @@ static void event_handler(struct esb_evt const *event) {
     app_esb_event_t m_event;
     switch (event->evt_id) {
         case ESB_EVENT_TX_SUCCESS:
-            k_msgq_get(&m_msgq_tx_payloads_sent, NULL, K_NO_WAIT); // remove the sent payload from the sent queue
+            // k_msgq_get(&m_msgq_tx_payloads_sent, NULL, K_NO_WAIT); // remove the sent payload from the sent queue
             tx_attempts = 0;
             // LOG_DBG("TX SUCCESS, tx_attempts: %d", event->tx_attempts);
             // LOG_DBG("give d1");
@@ -115,15 +115,15 @@ static void event_handler(struct esb_evt const *event) {
             break;
         case ESB_EVENT_TX_FAILED:
             struct esb_payload tx_payload;
-            k_msgq_get(&m_msgq_tx_payloads_sent, &tx_payload, K_NO_WAIT); // peek the failed payload from the sent queue
-            if (tx_attempts < CONFIG_ZMK_SPLIT_ESB_PROTO_TX_RETRANSMIT_COUNT) {
-                k_msgq_put(&m_msgq_tx_payloads, &tx_payload, K_NO_WAIT); // requeue it to the main queue
-                tx_attempts++;
-                LOG_WRN("TX FAILED, tx_attempts: %d", tx_attempts);
-            }
-            else {
-                tx_attempts = 0;
-            }
+            // k_msgq_get(&m_msgq_tx_payloads_sent, &tx_payload, K_NO_WAIT); // peek the failed payload from the sent queue
+            // if (tx_attempts < CONFIG_ZMK_SPLIT_ESB_PROTO_TX_RETRANSMIT_COUNT) {
+            //     k_msgq_put(&m_msgq_tx_payloads, &tx_payload, K_NO_WAIT); // requeue it to the main queue
+            //     tx_attempts++;
+            //     LOG_WRN("TX FAILED, tx_attempts: %d", tx_attempts);
+            // }
+            // else {
+            //     tx_attempts = 0;
+            // }
             // esb_flush_tx(); // DOUH, had fixed @ 3.1.0-rc1, not ready yet.
             // Forward an event to the application
             m_event.evt_type = APP_ESB_EVT_TX_FAIL;
@@ -235,7 +235,7 @@ static int pull_packet_from_tx_msgq(void) {
     struct esb_payload tx_payload;
     // bool tx_started = false;
 
-    if (k_msgq_get(&m_msgq_tx_payloads, &tx_payload, K_NO_WAIT) == 0) {
+    while (k_msgq_get(&m_msgq_tx_payloads, &tx_payload, K_NO_WAIT) == 0) {
         ret = esb_write_payload(&tx_payload);
 
         if (ret == 0)
