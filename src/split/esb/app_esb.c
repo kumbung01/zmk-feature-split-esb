7a4bc69494f28e7e90919bb21c50e69cac278918
seung <kumbung01@naver.com>
2025-08-27 18:27:29 +0900

    change algo

--- src/split/esb/app_esb.c
+++ src/split/esb/app_esb.c
@@ -117,7 +117,7 @@ static void event_handler(struct esb_evt const *event) {
             struct esb_payload tx_payload;
             k_msgq_get(&m_msgq_tx_payloads_sent, &tx_payload, K_NO_WAIT); // peek the failed payload from the sent queue
             if (tx_attempts < CONFIG_ZMK_SPLIT_ESB_PROTO_TX_RETRANSMIT_COUNT) {
-                k_msgq_put(&m_msgq_tx_payloads, &tx_payload, K_NO_WAIT); // requeue it to the main queue
+                k_msgq_put_front(&m_msgq_tx_payloads, &tx_payload, K_NO_WAIT); // requeue it to the main queue
                 tx_attempts++;
                 LOG_WRN("TX FAILED, tx_attempts: %d", tx_attempts);
             }
@@ -243,12 +243,11 @@ static int pull_packet_from_tx_msgq(void) {
     // static uint8_t que_was_fulled = 0;
     bool tx_started = false;
 
-    while (k_msgq_peek(&m_msgq_tx_payloads, &tx_payload) == 0) {
+    while (k_msgq_get(&m_msgq_tx_payloads, &tx_payload, K_NO_WAIT) == 0) {
         ret = esb_write_payload(&tx_payload);
 
         if (ret == 0)
         {
-            k_msgq_get(&m_msgq_tx_payloads, &tx_payload, K_NO_WAIT);
             k_msgq_put(&m_msgq_tx_payloads_sent, &tx_payload, K_NO_WAIT);
             if (!tx_started)
             {
@@ -266,15 +265,16 @@ static int pull_packet_from_tx_msgq(void) {
                 // msg size too large, discard it
                 LOG_WRN("esb_tx_fifo: tx_payload size too large (%d) > CONFIG_ESB_MAX_PAYLOAD_LENGTH (%d)",
                         tx_payload.length, CONFIG_ESB_MAX_PAYLOAD_LENGTH);
-                k_msgq_get(&m_msgq_tx_payloads, &tx_payload, K_NO_WAIT);
             }
             else if (ret == -ENOMEM)
             {
                 LOG_WRN("esb_tx_fifo: esb tx fifo full");
+                k_msgq_put_front(&m_msgq_tx_payloads_sent, &tx_payload, K_NO_WAIT);
                 goto exit_pull;
             }
             else {
                 LOG_DBG("other errors, retry later");
+                k_msgq_put_front(&m_msgq_tx_payloads_sent, &tx_payload, K_NO_WAIT);
                 goto exit_pull;
             }
         }
