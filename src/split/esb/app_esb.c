fb73065e0d9b7f5eb301702ebc23d93a92260a52
seung <kumbung01@naver.com>
2025-08-29 00:07:38 +0900

    add comment

--- src/split/esb/app_esb.c
+++ src/split/esb/app_esb.c
@@ -304,7 +304,7 @@ int zmk_split_esb_send(app_esb_data_t *tx_packet) {
     static struct esb_payload tx_payload;
 
 #if IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-    tx_payload.pipe = ((struct esb_command_envelope*)tx_packet->data)->payload.source;
+    tx_payload.pipe = ((struct esb_command_envelope*)tx_packet->data)->payload.source; // this should match tx FIFO pipe
     // tx_payload.pipe = 0; // use pipe 0 for central role
 #else
     tx_payload.pipe = CONFIG_ZMK_SPLIT_ESB_PERIPHERAL_ID; // use the peripheral_id as the ESB pipe number, offset by 1
