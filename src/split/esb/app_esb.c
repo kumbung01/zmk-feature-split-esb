fdf0de332796d726683337e20601606c23468515
seung <kumbung01@naver.com>
2025-08-31 22:54:24 +0900

    erase flush tx

--- src/split/esb/app_esb.c
+++ src/split/esb/app_esb.c
@@ -83,10 +83,6 @@ static void event_handler(struct esb_evt const *event) {
         case ESB_EVENT_TX_FAILED:
             // Forward an event to the application
             m_event.evt_type = APP_ESB_EVT_TX_FAIL;
-            if (m_mode == APP_ESB_MODE_PTX)
-            {
-                esb_flush_tx();
-            }
             m_callback(&m_event);
             pull_packet_from_tx_msgq();
             break;
