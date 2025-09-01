ca096c1c683a3e406a7e40317889fcab07d60d22
seung <kumbung01@naver.com>
2025-09-01 21:17:15 +0900

    change to local variable

--- src/split/esb/app_esb.c
+++ src/split/esb/app_esb.c
@@ -121,7 +121,7 @@ static void event_handler(struct esb_evt const *event) {
             break;
         case ESB_EVENT_RX_RECEIVED:
             // LOG_DBG("RX SUCCESS");
-            static struct esb_payload rx_payload = {0};
+            struct esb_payload rx_payload = {0};
             if (esb_read_rx_payload(&rx_payload) == 0) {
                 // LOG_DBG("Chunk %d, len: %d", rx_payload.pid, rx_payload.length);
                 LOG_DBG("RX pipe: %d", rx_payload.pipe);
