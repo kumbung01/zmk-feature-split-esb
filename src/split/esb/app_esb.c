4011b099898682865ebed6876bf59155806450ad
seung <kumbung01@naver.com>
2025-09-02 20:07:23 +0900

    change to msleep

--- src/split/esb/app_esb.c
+++ src/split/esb/app_esb.c
@@ -93,6 +93,12 @@ static void reset_retransmit_delay(void)
     }
 }
 
+int simple_random_bit(void) {
+    static unsigned int seed = 123456789;
+    seed = (1103515245 * seed + 12345) % (1u << 31);
+    return seed & 0x1;
+}
+
 static void event_handler(struct esb_evt const *event) {
     app_esb_event_t m_event = {0};
     switch (event->evt_id) {
@@ -101,21 +107,14 @@ static void event_handler(struct esb_evt const *event) {
             // Forward an event to the application
             m_event.evt_type = APP_ESB_EVT_TX_SUCCESS;
 
-            reset_retransmit_delay();
-
             m_callback(&m_event);
-            if(m_mode == APP_ESB_MODE_PTX)
-                pull_packet_from_tx_msgq();
-
+            pull_packet_from_tx_msgq();
             break;
         case ESB_EVENT_TX_FAILED:
             // Forward an event to the application
             m_event.evt_type = APP_ESB_EVT_TX_FAIL;
 
-            if (m_mode == APP_ESB_MODE_PTX)
-            {
-                inc_retransmit_delay();
-            }
+            k_msleep(1 + simple_random_bit());
 
             m_callback(&m_event);
             pull_packet_from_tx_msgq();
