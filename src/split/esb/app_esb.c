3158579fc623f42ca94f7e260facee6f21e5ea0f
seung <kumbung01@naver.com>
2025-09-04 18:53:07 +0900

    change algo

--- src/split/esb/app_esb.c
+++ src/split/esb/app_esb.c
@@ -230,10 +230,11 @@ static void set_tx_power(void)
     const int8_t pwr_min = -16;
     const int8_t pwr_max = 4;
 
-    int diff = rssi_target - rssi_now;
+    int diff = rssi_now - rssi-target;
     int8_t pwr = pwr_now;
 
     LOG_WRN("RSSI: %d dbm", rssi_now);
+    LOG_WRN("diff: %d dbm", diff);
 
     if (diff >= 2) {
         pwr = pwr - 2 <= pwr_min ? pwr_min : pwr - 2;
