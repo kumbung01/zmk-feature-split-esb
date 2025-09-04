faeb3b7596243d03be6b1bc2347e6893eb40eec7
seung <kumbung01@naver.com>
2025-09-04 22:43:04 +0900

    change dbm

--- src/split/esb/app_esb.c
+++ src/split/esb/app_esb.c
@@ -180,6 +180,7 @@ static int esb_initialize(app_esb_mode_t mode) {
     config.mode = (mode == APP_ESB_MODE_PTX) ? ESB_MODE_PTX : ESB_MODE_PRX;
     config.tx_mode = ESB_TXMODE_MANUAL_START;
     config.selective_auto_ack = true;
+    config.tx_output.power = (mode == APP_ESB_MODE_PTX) ? -4 : 0;
 
     err = esb_init(&config);
 
