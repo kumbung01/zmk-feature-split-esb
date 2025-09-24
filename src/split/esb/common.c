7fcd806169641a525a96844d18d387e714a722bb
seung <kumbung01@naver.com>
2025-09-24 20:13:35 +0900

    change prio

--- src/split/esb/common.c
+++ src/split/esb/common.c
@@ -30,7 +30,7 @@ void zmk_split_esb_cb(app_esb_event_t *event, struct zmk_split_esb_async_state *
             // pull_packet_from_tx_msgq();
             break;
         case APP_ESB_EVT_RX:
-            int ret = k_msgq_put(&rx_msgq, event->payload->data, K_FOREVER);
+            int ret = k_msgq_put(&rx_msgq, event->payload->data, K_NO_WAIT);
             if (ret) {
                 LOG_WRN("rx msgq put fail(%d)", ret);
                 break;
@@ -56,7 +56,7 @@ int service_init(void) {
     static const struct k_work_queue_config queue_config = {
         .name = "Split Peripheral Notification Queue"};
     k_work_queue_start(&rx_work_q, rx_work_q_stack, K_THREAD_STACK_SIZEOF(rx_work_q_stack),
-                       1, &queue_config);
+                       5, &queue_config);
 
     return 0;
 }
