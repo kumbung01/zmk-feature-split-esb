98f33bd533ada1148a467008be4deef307722c11
seung <kumbung01@naver.com>
2025-09-24 17:36:36 +0900

    change to forever

--- src/split/esb/common.c
+++ src/split/esb/common.c
@@ -27,7 +27,7 @@ void zmk_split_esb_cb(app_esb_event_t *event, struct zmk_split_esb_async_state *
             // pull_packet_from_tx_msgq();
             break;
         case APP_ESB_EVT_RX:
-            int ret = k_msgq_put(&rx_msgq, event->payload->data, K_NO_WAIT);
+            int ret = k_msgq_put(&rx_msgq, event->payload->data, K_FOREVER);
             if (ret) {
                 LOG_WRN("rx msgq put fail(%d)", ret);
                 break;
