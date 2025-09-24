c6e22bddd0b12570b71646bd24a42514861e21f9
seung <kumbung01@naver.com>
2025-09-25 02:17:30 +0900

    change prio

--- src/split/esb/central.c
+++ src/split/esb/central.c
@@ -188,5 +188,5 @@ static void publish_events_thread() {
 
 K_THREAD_DEFINE(publish_events_thread_id, STACKSIZE,
         publish_events_thread, NULL, NULL, NULL,
-        3, 0, 0);
+        1, 0, 0);
 
