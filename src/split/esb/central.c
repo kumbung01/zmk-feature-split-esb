cb6cdd95cc47c8ce0a560a7038c0f62e5509c6e9
seung <kumbung01@naver.com>
2025-09-25 18:42:36 +0900

    change rx thread to cooperative mode

--- src/split/esb/central.c
+++ src/split/esb/central.c
@@ -181,10 +181,11 @@ static void publish_events_thread() {
                                                             env.event.source,
                                                             env.event.event);
         }
+        k_yield();
     }
 }
 
 K_THREAD_DEFINE(publish_events_thread_id, STACKSIZE,
         publish_events_thread, NULL, NULL, NULL,
-        1, 0, 0);
+        K_PRIO_COOP(MPSL_THREAD_PRIO), 0, 0);
 
