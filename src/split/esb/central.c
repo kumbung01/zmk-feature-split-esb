b249aca8e57c7e04f72bb0d1303025a734f9c85d
seung <kumbung01@naver.com>
2025-10-14 05:38:03 +0900

    change yield location && decrease timeout ms

--- src/split/esb/central.c
+++ src/split/esb/central.c
@@ -158,6 +158,8 @@ static int break_packet(struct esb_payload *payload) {
 
         LOG_DBG("RX event type %d from source %d", evt.type, source);
         zmk_split_transport_central_peripheral_event_handler(&esb_central, source, evt);
+
+        k_yield();
     }
 
     return count;
@@ -172,7 +174,6 @@ static void publish_events_thread() {
 
         if (k_msgq_get(&rx_msgq, &payload, K_NO_WAIT) == 0) {
             break_packet(&payload);
-            k_yield();
         }   
     }
 }
